c7f6bcb9c50253b597807534da3b98ca86cae9c8
Valentin Waeselynck <val@bandsquare.fr>
2017-01-13 16:27:33 +0100

    *check-freshness* config var

--- src/vvvvalvalval/clj_inline_caching.clj
+++ src/vvvvalvalval/clj_inline_caching.clj
@@ -2,24 +2,36 @@
   (:require [vvvvalvalval.clj-inline-caching.impl :as impl :refer [cache*]])
   (:import [vvvvalvalval.clj_inline_caching.impl Cell]))
 
+(def ^:dynamic ^:redef *check-freshness*
+  "Whether or not to perform a cache freshness check.
+  Outside of interactive development, the cache should never become stale,
+  in which case you may want to set this Var to false for performance.
+
+  Note that this value will be accounted for at compile-time, not run-time."
+  true)
+
 (defmacro cached
   [keys-vec get-v]
   (let [n (count keys-vec)
         ks (for [i (range n)] (gensym (str "k" i)))
         cache-sym (vary-meta (gensym "cache") merge {:no-doc true})
+        read-cell `(let [cell# ~cache-sym]
+                     (if (instance? Cell cell#)             ;; at this point, cell# may be clojure.lang.Var&Unbound when using AOT compilation.
+                       cell#
+                       (impl/cache-var-miss* (var ~cache-sym))))
         _interned (intern *ns* cache-sym (Cell.))]
-    `(let [~@(interleave ks keys-vec)
-           getv# ~get-v
-           cell# (let [cell# ~cache-sym]
-                   (if (instance? Cell cell#) ;; at this point, cell# may be clojure.lang.Var&Unbound when using AOT compilation.
-                     cell#
-                     (impl/cache-var-miss* (var ~cache-sym))))]
-       (impl/cache* cell#
-         ~@(cond
-             (= n 0) ()
-             (<= 1 n impl/max-arity) ks
-             (< impl/max-arity n) [(vec ks)])
-         getv#))
+    (if *check-freshness*
+      `(let [~@(interleave ks keys-vec)
+             getv# ~get-v
+             cell# ~read-cell]
+         (impl/cache* cell#
+           ~@(cond
+               (= n 0) ()
+               (<= 1 n impl/max-arity) ks
+               (< impl/max-arity n) [(vec ks)])
+           getv#))
+      `(let [cell# ~read-cell]
+         (impl/cache* cell# ~get-v)))
     ))
 
 (defmacro call
